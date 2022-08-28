73ccb5d5a465082218c936e17a11d547941de1c8
David <david.lanyi101@gmail.com>
2022-08-28 14:10:56 -0400

    added spdlog

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <spdlog/spdlog.h>
 
 int main(){
+	spdlog::info("Welcome to spdlog!");
 	if (!glfwInit())
 		std::cout << "Error" << std::endl;
 	else
