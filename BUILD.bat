1b17893bc3892ad0cbf41605014f376c44930271
David <david.lanyi101@gmail.com>
2022-08-28 14:26:34 -0400

    fixed includes being saved on gh

--- BUILD.bat
+++ BUILD.bat
@@ -1,2 +1,7 @@
 CALL vendor\premake\premake5.exe vs2017
+CALL mkdir deps\include
+CALL xcopy /E /y deps\glad\include deps\include\
+CALL xcopy /E /y deps\glad\include deps\include\
+CALL xcopy /E /y deps\glfw\include deps\include\
+CALL xcopy /E /y deps\spdlog\include deps\include\
 PAUSE
